cb79ff4de4087e1843a8f71e14aa9d99f9fb5cb4
Michelle Mann <81351915+Michelle-Mann@users.noreply.github.com>
2023-06-09 07:47:38 -0700

    Add files via upload

---
+++ mannm3_rest (1)/exercise-controller.mjs
@@ -0,0 +1,84 @@
+import * as exercises from './exercise-model.mjs';
+import express from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.json());
+
+/**
+ * Create a new exercise with the name, # of reps, weight, unit and date provided in the body
+ */
+app.post('/exercises', (req, res) => {
+    
+    exercises.createExercise(req.body.name, req.body.reps, req.body.weight, req.body.unit, req.body.date)
+        .then(exercise => {
+            res.status(201).json(exercise);
+        })
+        .catch(error => {
+            console.error(error);
+            // In case of an error, send back status code 500 in case of an error.
+            // A better approach will be to examine the error and send an
+            // error status code corresponding to the error.
+            res.status(500).json({ Error: 'Request failed' });
+        });
+});
+
+/**
+ * Retrieve exercises. 
+ * All exercies are returned.
+ */
+app.get('/exercises', (req, res) => {
+    let filter = new Object();
+
+    exercises.findExercises(filter, '', 0)
+        .then(exercises => {
+            res.status(200).json(exercises);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({ Error: 'Request failed' });
+        });
+});
+
+/**
+ * Update the exercise whose id is provided in the path parameter and set
+ * its name, reps, weights, units and date of the exercise
+ */
+app.put('/exercises/:_id', (req, res) => {
+    exercises.replaceExercise(req.params._id, req.body.name, req.body.reps, req.body.weight, req.body.unit, req.body.date)
+        .then(numUpdated => {
+            if (numUpdated === 1) {
+                res.status(200).json({ _id: req.params._id, name: req.body.name, reps: req.body.reps, weight: req.body.weight, unit: req.body.unit, date: req.body.date })
+            } else {
+                res.status(500).json({ Error: 'Resource not found' });
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({ Error: 'Request failed' });
+        });
+});
+
+/**
+ * Delete the exercise whose id is provided in the query parameters
+ */
+app.delete('/exercises/:_id', (req, res) => {
+    exercises.deleteById(req.params._id)
+        .then(deletedCount => {
+            if (deletedCount === 1) {
+                res.status(204).send();
+            } else {
+                res.status(500).json({ Error: 'Resource not found' });
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({ error: 'Request failed' });
+        });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
